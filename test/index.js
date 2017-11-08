f2857c0ef579f62a5d6034de4c679590e76fac10
Takuma Hanatani <mail@potato4d.me>
2017-11-08 17:14:37 +0900

    Add prefix test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 import test from 'ava'
 import vind from '../lib'
 
-test('type: String', (t)=> {
+test('--type=string  --prefix=undefined', (t)=> {
   const obj = Object.create(null)
   obj['color'] = 'primary'
   obj['size'] = 'large'
@@ -17,7 +17,7 @@ test('type: String', (t)=> {
   t.is(result['is-large'], true)
 })
 
-test('type: Boolean', (t)=> {
+test('--type=boolean --prefix=undefined', (t)=> {
   const obj = Object.create(null)
   obj['ghost'] = true
   obj['submit'] = false
@@ -29,4 +29,34 @@ test('type: Boolean', (t)=> {
   t.is('is-submit' in result, true)
   t.is(result['is-ghost'], true)
   t.is(result['is-submit'], false)
+})
+
+test('--type=string  --prefix=--', (t)=> {
+  const obj = Object.create(null)
+  obj['color'] = 'primary'
+  obj['size'] = 'large'
+
+  const result = vind(obj, '--')
+  t.is(Object.keys(result).length, 2)
+
+  t.is('color' in result, false)
+  t.is('size' in result, false)
+  t.is('--color' in result, false)
+  t.is('--size' in result, false)
+  t.is(result['--primary'], true)
+  t.is(result['--large'], true)
+})
+
+test('--type=boolean --prefix=--', (t)=> {
+  const obj = Object.create(null)
+  obj['ghost'] = true
+  obj['submit'] = false
+
+  const result = vind(obj, '--')
+  t.is(Object.keys(result).length, 2)
+
+  t.is('--ghost' in result, true)
+  t.is('--submit' in result, true)
+  t.is(result['--ghost'], true)
+  t.is(result['--submit'], false)
 })
